b0774cad13c06f125afa852d2145c468c9b9df22
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2016-09-30 13:16:01 +0530

    Add files via upload

--- Games/general.js
+++ Games/general.js
@@ -31,3 +31,11 @@ window.onclick = function(event) {
       }
     }
   }}
+
+
+var overlay = document.getElementById("overlay");
+window.addEventListener("load", function(){
+    overlay.style.display="none";
+
+  })
+
