6d9cc5fccd944209ccfcfa3495b2d1e7536cb444
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2016-12-01 13:13:32 +0530

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -3,16 +3,16 @@ var pagetop, menu, yPos;
 yPos=0;
 function yScroll(){
 	pagetop = document.getElementById('pagetop');
-    menu = document.getElementById('navDiv');	
+  	
 	yPos = window.pageYOffset;
 	if(yPos > 125){
 		pagetop.style.height = "36px";
-	
+	menu.style.opacity = "0";
 	
 	} else {
 		pagetop.style.height = "100px";
 		pagetop.style.paddingTop = "50px";
-		menu.style.height = "50px";
+      
 	}
 }
 window.addEventListener("scroll", yScroll);
